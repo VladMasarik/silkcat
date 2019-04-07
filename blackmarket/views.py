0064804213b34199018956a2b5ce9737edfe1d3e
Vladimir Masarik <vmasarik@redhat.com>
2019-04-07 15:46:46 +0200

    fix location

--- blackmarket/views.py
+++ blackmarket/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 def index(request):
     context = {}
-    return HttpResponse(render(request, "bm/index.html", context))
+    return HttpResponse(render(request, "/home/ubuntu/silkcat/blackmarket/templates/bm/index.html", context))
