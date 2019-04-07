9af27d448e330c79ef6c5aeb8fa3685943931672
Vladimir Masarik <vmasarik@redhat.com>
2019-04-07 23:01:15 +0200

    added urls and views

--- blackmarket/urls.py
+++ blackmarket/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('upload', views.upload, name='upload'),
     path('<id>', views.detail, name='detail'),
     path('edit/<id>', views.edit, name='edit'),
-    path('upload', views.upload, name='upload'),
     path('delete/<id>', views.delete, name='delete'),
 ]
--- blackmarket/views.py
+++ blackmarket/views.py
@@ -2,15 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from blackmarket.models import Cat
 
-
+"""
+wizard
+long
+bongo
+grumpy
+https://i.imgur.com/AYTaKY9.jpg
+https://7gigzxopz0uiqxo1-zippykid.netdna-ssl.com/wp-content/uploads/2015/07/cat.jpg
+https://img.buzzfeed.com/buzzfeed-static/static/2018-10/5/12/asset/buzzfeed-prod-web-03/sub-buzz-29911-1538758090-1.jpg
+https://www.tapatalk.com/groups/animaluntamed/imageproxy.php?url=http://i917.photobucket.com/albums/ad19/JinenFordragon/541252ef91e16735b792945a4c260111e43225a.jpg
+https://cdn.themindcircle.com/wp-content/uploads/2017/08/funny-cat-fails-1.jpg
+https://sadanduseless.b-cdn.net/wp-content/uploads/2018/08/burrito3.jpg
+ninja
+box
+old school
+"""
 
 def index(request):
 
-    #cat = Cat.objects.create(name="wizard", size="small", color="red", image_path="/path")
-
-    
-    context = {}
-
+    cats = Cat.objects.all()
+    context = {
+        "cat_list": cats,
+    }
 
     return HttpResponse(render(request, "bm/index.html", context))
 
@@ -34,7 +47,8 @@ def edit(request, id):
 
 
 
-def upload(request, id):
+def upload(request):
+    Cat.objects.create(name="wizard", size="small", color="red", image_path="liquid.jpg")
     context = {}
     return HttpResponse(render(request, "bm/upload.html", context))
 
