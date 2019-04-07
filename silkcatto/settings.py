a9939012c8d971c90988c2e505014caf93b1c5bc
Vladimir Masarik <vmasarik@redhat.com>
2019-04-07 15:40:48 +0200

    added allowed hosts

--- silkcatto/settings.py
+++ silkcatto/settings.py
@@ -10,6 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+
+"""
+wizard
+long
+bongo
+grumpy
+https://i.imgur.com/AYTaKY9.jpg
+https://7gigzxopz0uiqxo1-zippykid.netdna-ssl.com/wp-content/uploads/2015/07/cat.jpg
+https://img.buzzfeed.com/buzzfeed-static/static/2018-10/5/12/asset/buzzfeed-prod-web-03/sub-buzz-29911-1538758090-1.jpg
+https://www.tapatalk.com/groups/animaluntamed/imageproxy.php?url=http://i917.photobucket.com/albums/ad19/JinenFordragon/541252ef91e16735b792945a4c260111e43225a.jpg
+https://cdn.themindcircle.com/wp-content/uploads/2017/08/funny-cat-fails-1.jpg
+https://sadanduseless.b-cdn.net/wp-content/uploads/2018/08/burrito3.jpg
+ninja
+box
+old school
+"""
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -25,7 +42,7 @@ SECRET_KEY = 'cx4%9f3xqgtrlz=50vjhx56np$2rt2@@5(qn1fe&p%rg(gn8k&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['193.196.52.69', '127.0.0.1']
 
 
 # Application definition
